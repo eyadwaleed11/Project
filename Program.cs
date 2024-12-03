47d56e7278a26f299bbda773e12de1964378805b
eyadwaleed11 <105245754+eyadwaleed11@users.noreply.github.com>
2024-12-04 01:06:02 +0200

    finished

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // Commented out to disable HTTPS redirection
+app.UseAuthorization();
+app.MapControllers();
 
 var summaries = new[]
 {
@@ -31,7 +38,8 @@ app.MapGet("/weatherforecast", () =>
         .ToArray();
     return forecast;
 })
-.WithName("GetWeatherForecast");
+.WithName("GetWeatherForecast")
+.WithOpenApi();
 
 app.Run();
 
