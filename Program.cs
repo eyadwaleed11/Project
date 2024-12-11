387d6931951fa8a7643d830d0146ef2ccbb971d1
eyadwaleed11 <105245754+eyadwaleed11@users.noreply.github.com>
2024-12-11 02:36:03 +0200

    semiDONE

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Project;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AdventureWorks2022Context>(options =>
+{ var ConnectionString = builder.Configuration.GetConnectionString("AdventureWorks2022");
+    options.UseSqlServer(ConnectionString);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590f910cf396aaa1682c951bdfb586288e7b4ee2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784124ef59b483e73935ceadfaf56f6392047a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
